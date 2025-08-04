77818dc3d17d46cd2586e19167c572c04b51b446
inkosurf <36050444+inkosurf@users.noreply.github.com>
2025-08-04 00:11:56 -0300

    Update main.py

--- main.py
+++ main.py
@@ -28,10 +28,7 @@ def index():
         return render_template("index.html", link_resultado="static/final.png")
 
     return render_template("index.html")
-if __name__ == '__main__':
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+
 
 
 
